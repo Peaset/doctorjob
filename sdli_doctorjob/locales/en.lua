6b13b07b19f60eee903a9d81d605603495e75c7d
Peaset <76628978+Peaset@users.noreply.github.com>
2021-01-17 13:30:09 +0300

    Add files via upload

---
+++ sdli_doctorjob/locales/en.lua
@@ -0,0 +1,14 @@
+Locales['en'] = {
+    ['titulo'] = 'Doktor',
+    ['subtitulo'] = 'Menü',
+    ['sub_menu'] = 'At',
+    ['sub_menu2'] = 'Seçenekler',
+    ['sub_menu3'] = 'Ekipman',
+    ['opciones'] = 'Seçenekler',
+    ['utensilios'] = 'Araçlar',
+    ['caballo'] = 'At',
+    ['revivir'] = 'Onarmak',
+    ['curar'] = 'Bakım',
+    ['curar2'] = 'Kendine iyi bak',
+    ['lazo'] = 'Periyot',
+}
